999fa441f162e8edea5acf8264c466a47605eca0
Josh Soref <jsoref@users.noreply.github.com>
2021-11-04 00:35:18 -0400

    spelling: arbitrary

--- bench/tests/deltablue.lua
+++ bench/tests/deltablue.lua
@@ -222,7 +222,7 @@ end
 --
 -- Normal constraints are not input constraints.  An input constraint
 -- is one that depends on external state, such as the mouse, the
--- keybord, a clock, or some arbitraty piece of imperative code.
+-- keybord, a clock, or some arbitrary piece of imperative code.
 --
 function Constraint:isInput ()
 	 return false
