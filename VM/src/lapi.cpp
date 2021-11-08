0fbc918889eee48bc1e1cbff1682474cd1e7bcaa
LoganDark <logandarklock@gmail.com>
2021-11-08 12:30:54 -0800

    api_check

--- VM/src/lapi.cpp
+++ VM/src/lapi.cpp
@@ -704,8 +704,7 @@ void lua_setreadonly(lua_State* L, int objindex, bool value)
     const TValue* o = index2adr(L, objindex);
     api_check(L, ttistable(o));
     Table* t = hvalue(o);
-    if (value && t == hvalue(registry(L)))
-        luaG_runerror(L, "Attempt to set the registry to readonly");
+    api_check(L, t != hvalue(registry(L)));
     t->readonly = value;
     return;
 }
