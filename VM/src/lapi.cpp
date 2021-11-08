ddc6ae749188c4df1c5ca67ff3e77a1abbb27e29
LoganDark <logandarklock@gmail.com>
2021-11-08 12:11:00 -0800

    Don't allow registry to be set to readonly

--- VM/src/lapi.cpp
+++ VM/src/lapi.cpp
@@ -10,6 +10,7 @@
 #include "ldo.h"
 #include "lvm.h"
 #include "lnumutils.h"
+#include "ldebug.h"
 
 #include <string.h>
 
@@ -703,6 +704,8 @@ void lua_setreadonly(lua_State* L, int objindex, bool value)
     const TValue* o = index2adr(L, objindex);
     api_check(L, ttistable(o));
     Table* t = hvalue(o);
+    if (value && obj2gco(t) == gcvalue(registry(L)))
+        luaG_runerror(L, "Attempt to set the registry to readonly");
     t->readonly = value;
     return;
 }
