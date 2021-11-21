a423e24dbf670ff750d7d933f268b40bf06adda2
Pelanyo Kamara <pkamara@disroot.org>
2021-11-21 23:31:20 +0000

    inline redundant variable

--- VM/src/laux.cpp
+++ VM/src/laux.cpp
@@ -189,8 +189,7 @@ LUALIB_API bool luaL_checkboolean(lua_State* L, int narg)
     // all other truthy/falsy values. If the desired result
     // is true if value is present then lua_toboolean should
     // directly be used instead.
-    const auto isbool = lua_isboolean(L, narg);
-    if (!isbool)
+    if (!lua_isboolean(L, narg))
         tag_error(L, narg, LUA_TBOOLEAN);
     return lua_toboolean(L, narg);
 }
