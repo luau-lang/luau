677994b243176c538f1f936b78604420095daed9
Petri Häkkinen <petrihakkinen@users.noreply.github.com>
2021-11-30 18:14:28 +0200

    Fix: luaL_sandbox leaves value on the stack (#253)
    Co-authored-by: Petri Häkkinen <petrih@rmd.remedy.fi>

--- VM/src/linit.cpp
+++ VM/src/linit.cpp
@@ -44,7 +44,7 @@ void luaL_sandbox(lua_State* L)
     lua_pushliteral(L, "");
     lua_getmetatable(L, -1);
     lua_setreadonly(L, -1, true);
-    lua_pop(L, 1);
+    lua_pop(L, 2);
 
     // set globals to readonly and activate safeenv since the env is immutable
     lua_setreadonly(L, LUA_GLOBALSINDEX, true);
