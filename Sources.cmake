acbcc807e88dd246be817c135fec62b3590ce4c0
Lily Brown <lbrown@roblox.com>
2022-02-07 11:37:57 -0800

    add Luau.Ast.CLI target

--- Sources.cmake
+++ Sources.cmake
@@ -193,6 +193,13 @@ if(TARGET Luau.Analyze.CLI)
         CLI/Analyze.cpp)
 endif()
 
+if (TARGET Luau.Ast.CLI)
+    target_sources(Luau.Ast.CLI PRIVATE
+        CLI/FileUtils.h
+        CLI/FileUtils.cpp
+        CLI/Ast.cpp)
+endif()
+
 if(TARGET Luau.UnitTest)
     # Luau.UnitTest Sources
     target_sources(Luau.UnitTest PRIVATE
