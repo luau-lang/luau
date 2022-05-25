ce4066f1a35438e8b969636fd5122a92be130ca7
Allan Jeremy <aj.dev254@gmail.com>
2022-05-25 15:38:10 +0300

    Added measure time script that can receive a bash command as an argument and measure the amount of time required to run a script

---
+++ bench/measure_time.py
@@ -0,0 +1,29 @@
+import os, sys, time
+try:
+    import  numpy as np
+    from scipy import mean, stats
+except ModuleNotFoundError:
+    print("Warning: scipy package is not installed, confidence values will not be available")
+    stats = None
+
+duration_list = []
+
+for i in range(1,10):
+  start = time.perf_counter()
+
+  print(sys.argv[1])
+
+  os.system(sys.argv[1])
+
+  end = time.perf_counter()
+
+  duration_ms = (end - start) * 1000
+
+  duration_list.append(duration_ms)
+
+
+# Stats
+mean = np.mean(duration_list)
+std_err = stats.sem(duration_list)
+
+print("SUCCESS: {} : {:.2f}ms +/- {:.2f}% on luau ".format('duration', mean,std_err))
