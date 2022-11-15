45ab1105a6918fcd910603d4f71347caa9f4e812
Kampfkarren <boynedmaster@gmail.com>
2022-11-15 13:14:17 -0800

    Not necessary

--- Analysis/src/AstQuery.cpp
+++ Analysis/src/AstQuery.cpp
@@ -1,7 +1,6 @@
 // This file is part of the Luau programming language and is licensed under MIT License; see LICENSE.txt for details
 #include "Luau/AstQuery.h"
 
-#include "Luau/Ast.h"
 #include "Luau/Module.h"
 #include "Luau/Scope.h"
 #include "Luau/TypeInfer.h"
