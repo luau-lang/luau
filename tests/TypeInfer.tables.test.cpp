118f6ed3c6d92a3c179ef02611ff215d21a2e70f
JohnnyMorganz <johnnymorganz@outlook.com>
2023-07-03 15:24:10 +0100

    Add test case

--- tests/TypeInfer.tables.test.cpp
+++ tests/TypeInfer.tables.test.cpp
@@ -3642,4 +3642,34 @@ end
     LUAU_REQUIRE_NO_ERRORS(result);
 }
 
+TEST_CASE_FIXTURE(BuiltinsFixture, "write_common_property_to_table_intersection_1")
+{
+
+    CheckResult result = check(R"(
+type Foo = {
+	Bar: string,
+} & { Baz: number }
+
+local x: Foo = { Bar = "1", Baz = 2 }
+local y = x.Bar
+    )");
+
+    LUAU_REQUIRE_NO_ERRORS(result);
+}
+
+TEST_CASE_FIXTURE(BuiltinsFixture, "write_common_property_to_table_intersection_2")
+{
+
+    CheckResult result = check(R"(
+type Foo = {
+	Bar: string,
+} & { Baz: number }
+
+local x: Foo = { Bar = "1", Baz = 2 }
+local y = x["Bar"]
+    )");
+
+    LUAU_REQUIRE_NO_ERRORS(result);
+}
+
 TEST_SUITE_END();
