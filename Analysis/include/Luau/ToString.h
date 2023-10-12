1173e415ef705dfc66fcc6198e9ed155161fdc6c
JohnnyMorganz <johnnymorganz@outlook.com>
2023-10-12 18:09:47 +0200

    Modify the way line breaks are added when printing unions/intersections (#1047)
    Adds an option `compositeTypesSingleLineLimit` to control when
    unions/intersections are broken up onto multiple lines when printed.
    Simple unions/intersections will remain on one line. Default value is
    set to `5`.
    
    I took the liberty to always expand overloaded function intersections on
    multiple lines, as I believe they are best viewed that way, even if
    theres only 2 functions.
    
    Closes #963

--- Analysis/include/Luau/ToString.h
+++ Analysis/include/Luau/ToString.h
@@ -11,6 +11,7 @@
 
 LUAU_FASTINT(LuauTableTypeMaximumStringifierLength)
 LUAU_FASTINT(LuauTypeMaximumStringifierLength)
+LUAU_FASTFLAG(LuauToStringSimpleCompositeTypesSingleLine)
 
 namespace Luau
 {
@@ -39,6 +40,12 @@ struct ToStringNameMap
 
 struct ToStringOptions
 {
+    ToStringOptions(bool exhaustive = false)
+        : exhaustive(exhaustive)
+        , compositeTypesSingleLineLimit(FFlag::LuauToStringSimpleCompositeTypesSingleLine ? 5 : 0)
+    {
+    }
+
     bool exhaustive = false;                      // If true, we produce complete output rather than comprehensible output
     bool useLineBreaks = false;                   // If true, we insert new lines to separate long results such as table entries/metatable.
     bool functionTypeArguments = false;           // If true, output function type argument names when they are available
@@ -47,6 +54,7 @@ struct ToStringOptions
     bool hideFunctionSelfArgument = false;        // If true, `self: X` will be omitted from the function signature if the function has self
     size_t maxTableLength = size_t(FInt::LuauTableTypeMaximumStringifierLength); // Only applied to TableTypes
     size_t maxTypeLength = size_t(FInt::LuauTypeMaximumStringifierLength);
+    size_t compositeTypesSingleLineLimit = 5; // The number of type elements permitted on a single line when printing type unions/intersections
     ToStringNameMap nameMap;
     std::shared_ptr<Scope> scope; // If present, module names will be added and types that are not available in scope will be marked as 'invalid'
     std::vector<std::string> namedFunctionOverrideArgNames; // If present, named function argument names will be overridden
