bbe06c00c67f83d63674b6fa8f8f54117ce692db
Petri Häkkinen <petrihakkinen@users.noreply.github.com>
2023-11-10 15:51:05 +0200

    Update tests/Compiler.test.cpp
    Co-authored-by: vegorov-rbx <75688451+vegorov-rbx@users.noreply.github.com>

--- tests/Compiler.test.cpp
+++ tests/Compiler.test.cpp
@@ -4498,7 +4498,7 @@ TEST_CASE("VectorLiterals")
 {
     ScopedFastFlag sff("LuauVectorLiterals", true);
 
-    CHECK_EQ("\n" + compileFunction("return Vector3.new(1, 2, 3)", 0, 2, true), R"(
+    CHECK_EQ("\n" + compileFunction("return Vector3.new(1, 2, 3)", 0, 2, /*enableVectors*/ true), R"(
 LOADK R0 K0 [1, 2, 3]
 RETURN R0 1
 )");
