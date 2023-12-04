805be8e231380adb0c9cafa86c4f8cf3aa0d82c3
Vighnesh <vvijay@roblox.com>
2023-12-04 15:39:48 -0800

    lowercase

--- tests/RequireByString.test.cpp
+++ tests/RequireByString.test.cpp
@@ -66,8 +66,8 @@ public:
                     luauDirRel += "/Client";
                     luauDirAbs += "/Client";
                 }
-                luauDirRel += "/Luau";
-                luauDirAbs += "/Luau";
+                luauDirRel += "/luau";
+                luauDirAbs += "/luau";
 
                 if (type == PathType::Relative)
                     return luauDirRel;
