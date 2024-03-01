d7052281d57e7d0124e7386b18a54acaf86d449f
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2024-02-29 17:01:58 -0800

    Add a/a and a-a cases as well

--- CodeGen/src/IrLoweringX64.cpp
+++ CodeGen/src/IrLoweringX64.cpp
@@ -624,7 +624,7 @@ void IrLoweringX64::lowerInst(IrInst& inst, uint32_t index, const IrBlock& next)
         ScopedRegX64 tmp2{regs};
 
         RegisterX64 tmpa = vecOp(inst.a, tmp1);
-        RegisterX64 tmpb = vecOp(inst.b, tmp2);
+        RegisterX64 tmpb = (inst.a == inst.b) ? tmpa : vecOp(inst.b, tmp2);
 
         build.vsubps(inst.regX64, tmpa, tmpb);
         if (!FFlag::LuauCodegenVectorTag)
@@ -654,7 +654,7 @@ void IrLoweringX64::lowerInst(IrInst& inst, uint32_t index, const IrBlock& next)
         ScopedRegX64 tmp2{regs};
 
         RegisterX64 tmpa = vecOp(inst.a, tmp1);
-        RegisterX64 tmpb = vecOp(inst.b, tmp2);
+        RegisterX64 tmpb = (inst.a == inst.b) ? tmpa : vecOp(inst.b, tmp2);
 
         build.vdivps(inst.regX64, tmpa, tmpb);
         if (!FFlag::LuauCodegenVectorTag)
