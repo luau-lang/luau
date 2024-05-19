640ead1b9ecd82871cac375a4f7f9ee33188d745
Alexander McCord <alex@mccord.dev>
2024-05-19 11:38:11 -0700

    Not sure why Language was templated. Absent-minded me.

--- EqSat/include/Luau/Language.h
+++ EqSat/include/Luau/Language.h
@@ -1,12 +1,9 @@
 // This file is part of the Luau programming language and is licensed under MIT License; see LICENSE.txt for details
 #pragma once
 
-#include "Luau/Id.h"
-
 namespace Luau::EqSat
 {
 
-template<typename L>
 struct Language
 {
 
