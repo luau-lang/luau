3d9c7cb5c08b2869ae96a12a83755d85cc6759a9
Alexander McCord <alex@mccord.dev>
2024-06-08 09:06:57 -0700

    Need to include cstddef.

--- EqSat/include/Luau/LanguageHash.h
+++ EqSat/include/Luau/LanguageHash.h
@@ -1,6 +1,7 @@
 // This file is part of the Luau programming language and is licensed under MIT License; see LICENSE.txt for details
 #pragma once
 
+#include <cstddef>
 #include <functional>
 
 namespace Luau::EqSat
