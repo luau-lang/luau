d9536cecd8cbaae5f6f76af79209418f0a78efaf
Andy Friesen <afriesen@roblox.com>
2024-09-06 13:34:50 -0700

    Sync to upstream/release/642 (#1385)
    ## New Solver
    
    * The type functions `keyof` and `index` now also walk the inheritance
    chain when they are used on class types like Roblox instances.
    
    ---------
    
    Co-authored-by: Aaron Weiss <aaronweiss@roblox.com>

--- tests/TypeInfer.builtins.test.cpp
+++ tests/TypeInfer.builtins.test.cpp
@@ -793,9 +793,7 @@ TEST_CASE_FIXTURE(BuiltinsFixture, "select_with_variadic_typepack_tail_and_strin
 
 TEST_CASE_FIXTURE(Fixture, "string_format_as_method")
 {
-    // CLI-115690
-    if (FFlag::LuauSolverV2)
-        return;
+    ScopedFastFlag sff{FFlag::LuauDCRMagicFunctionTypeChecker, true};
 
     CheckResult result = check("local _ = ('%s'):format(5)");
 
@@ -809,6 +807,7 @@ TEST_CASE_FIXTURE(Fixture, "string_format_as_method")
 
 TEST_CASE_FIXTURE(Fixture, "string_format_use_correct_argument")
 {
+    ScopedFastFlag sff{FFlag::LuauDCRMagicFunctionTypeChecker, true};
     CheckResult result = check(R"(
         local _ = ("%s"):format("%d", "hello")
     )");
@@ -820,10 +819,7 @@ TEST_CASE_FIXTURE(Fixture, "string_format_use_correct_argument")
 
 TEST_CASE_FIXTURE(Fixture, "string_format_use_correct_argument2")
 {
-    // CLI-115690
-    if (FFlag::LuauSolverV2)
-        return;
-
+    ScopedFastFlag sff{FFlag::LuauDCRMagicFunctionTypeChecker, true};
     CheckResult result = check(R"(
         local _ = ("%s %d").format("%d %s", "A type error", 2)
     )");
@@ -882,10 +878,7 @@ TEST_CASE_FIXTURE(BuiltinsFixture, "debug_info_is_crazy")
 
 TEST_CASE_FIXTURE(BuiltinsFixture, "aliased_string_format")
 {
-    // CLI-115690
-    if (FFlag::LuauSolverV2)
-        return;
-
+    ScopedFastFlag sff{FFlag::LuauDCRMagicFunctionTypeChecker, true};
     CheckResult result = check(R"(
         local fmt = string.format
         local s = fmt("%d", "oops")
@@ -945,10 +938,6 @@ TEST_CASE_FIXTURE(BuiltinsFixture, "select_on_variadic")
 
 TEST_CASE_FIXTURE(BuiltinsFixture, "string_format_report_all_type_errors_at_correct_positions")
 {
-    // CLI-115690
-    if (FFlag::LuauSolverV2)
-        return;
-
     ScopedFastFlag sff{FFlag::LuauDCRMagicFunctionTypeChecker, true};
     CheckResult result = check(R"(
         ("%s%d%s"):format(1, "hello", true)
