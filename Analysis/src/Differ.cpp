b76941b6785aca53ecafaef7eb165dbc6ea23fc3
Vighnesh <vvijay@roblox.com>
2024-10-11 15:21:25 -0700

    annotate this optional use

--- Analysis/src/Differ.cpp
+++ Analysis/src/Differ.cpp
@@ -944,11 +944,15 @@ std::vector<std::pair<TypeId, TypeId>>::const_reverse_iterator DifferEnvironment
     return visitingStack.crend();
 }
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
 DifferResult diff(TypeId ty1, TypeId ty2)
 {
     DifferEnvironment differEnv{ty1, ty2, std::nullopt, std::nullopt};
     return diffUsingEnv(differEnv, ty1, ty2);
 }
+#pragma GCC diagnostic pop      
+
 
 DifferResult diffWithSymbols(TypeId ty1, TypeId ty2, std::optional<std::string> symbol1, std::optional<std::string> symbol2)
 {
