03715edca882066fc3f2659082f8c08185fe23ce
Hunter Goldstein <hgoldstein@roblox.com>
2025-12-12 11:10:46 -0800

    Missing license notice in new files.

--- Analysis/src/IterativeTypeVisitor.cpp
+++ Analysis/src/IterativeTypeVisitor.cpp
@@ -1,4 +1,4 @@
-
+// This file is part of the Luau programming language and is licensed under MIT License; see LICENSE.txt for details
 #include "Luau/IterativeTypeVisitor.h"
 
 LUAU_FASTINT(LuauVisitRecursionLimit)
