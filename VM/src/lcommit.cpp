ace371005a55779a03dece1e5e3af9762b70db64
PhoenixWhitefire <86601049+PhoenixWhitefire@users.noreply.github.com>
2026-06-06 09:58:14 +0530

    Track `lcommit.cpp`

---
+++ VM/src/lcommit.cpp
@@ -0,0 +1,21 @@
+// This file is part of the Luau programming language and is licensed under MIT License; see LICENSE.txt for details
+// This code is based on Lua 5.x implementation licensed under MIT License; see lua_LICENSE.txt for details
+#include "lua.h"
+
+#ifndef LCOMMIT_CPP_COMMIT_HASH
+#define LCOMMIT_CPP_COMMIT_HASH "unknown"
+#endif
+
+#ifndef LCOMMIT_CPP_COMMIT_TAG
+#define LCOMMIT_CPP_COMMIT_TAG "unknown"
+#endif
+
+const char* luau_getcommithash()
+{
+    return LCOMMIT_CPP_COMMIT_HASH;
+}
+
+const char* luau_getcommittag()
+{
+    return LCOMMIT_CPP_COMMIT_TAG;
+}
