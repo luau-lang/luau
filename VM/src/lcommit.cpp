a02eb90022109accbd49c6d877316faca0b9f0ee
PhoenixWhitefire <86601049+PhoenixWhitefire@users.noreply.github.com>
2026-06-06 11:48:16 +0530

    remove incorrect copied comment

--- VM/src/lcommit.cpp
+++ VM/src/lcommit.cpp
@@ -1,5 +1,4 @@
 // This file is part of the Luau programming language and is licensed under MIT License; see LICENSE.txt for details
-// This code is based on Lua 5.x implementation licensed under MIT License; see lua_LICENSE.txt for details
 #include "lua.h"
 
 #ifndef LCOMMIT_CPP_COMMIT_HASH
